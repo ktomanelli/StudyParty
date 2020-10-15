73b20ca3aa9b012e7aa3e8a70aade35487e0f216
ADC04 <allendcooper@gmail.com>
2020-10-15 11:13:19 -0400

    added javascript typewriter effect on subtitle

--- src/Pages/SignIn/SignIn.js
+++ src/Pages/SignIn/SignIn.js
@@ -10,6 +10,13 @@ class SignIn extends Component {
     email: "",
     password: "",
     error: null,
+    typed: "",
+  }
+
+  componentDidMount() {
+    const subtitleStr = 'Hey! You want to umm... study???'
+    setTimeout(()=> this.typeWriter(subtitleStr), 1500)
+
   }
 
   signInWithEmailAndPasswordHandler = (event, email, password) => {
@@ -30,6 +37,17 @@ class SignIn extends Component {
     }
   }
 
+  typeWriter(slicedStr) {
+    if (slicedStr.length !== 0) {
+      this.setState((state) => ({
+        typed: state.typed.concat(slicedStr[0])
+      }));
+
+      setTimeout(() => this.typeWriter(slicedStr.slice(1)), 50);
+      // console.log(`this.state.typed: ${this.state.typed}`)
+    }
+  }
+
   render() {
     console.log(this.props.loggedIn)
     return (
@@ -37,37 +55,42 @@ class SignIn extends Component {
         <NavigationBar changeLoggedIn={this.props.changeLoggedIn} loggedIn={this.props.loggedIn} />
         <Container style={{ backgroundColor: 'white' }}>
 
-          <Jumbotron style={{ backgroundColor: 'white', marginBottom: '0px'}}>
-            <h1 className="main-title permanent-marker pink animate__animated animate__pulse">StudyParty!</h1>
-            <h6 className="montserrat">Hey! You want to umm... study???</h6>
+          <Jumbotron style={{ backgroundColor: 'white', marginBottom: '0px' }}>
+            <h1 className="main-title permanent-marker pink animate__animated animate__bounceInDown">StudyParty!</h1>
+            <h6 className="montserrat">
+              {this.state.typed}
+              <span className="cursor">
+                <span style={{ fontWeight: "normal" }}>|</span>
+              </span>
+            </h6>
           </Jumbotron>
-          <div style={{maxWidth: "600px", margin: "0 auto"}}>
-          <Form>
-            <Form.Group controlId="userEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" name="userEmail" value={this.state.email} onChange = {(event) => this.onChangeHandler(event)} />
-              {/* <Form.Text className="text-muted">
+          <div style={{ maxWidth: "600px", margin: "0 auto" }}>
+            <Form>
+              <Form.Group controlId="userEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" name="userEmail" value={this.state.email} onChange={(event) => this.onChangeHandler(event)} />
+                {/* <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text> */}
-            </Form.Group>
+              </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" name="userPassword" value={this.state.password} onChange={(event) => this.onChangeHandler(event)} />
-            </Form.Group>
-            <Button variant="primary" type="submit" block>
-              Sign In
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" name="userPassword" value={this.state.password} onChange={(event) => this.onChangeHandler(event)} />
+              </Form.Group>
+              <Button variant="primary" type="submit" block>
+                Sign In
             </Button>
-            <div style={{margin: '40px 0px'}}>
-              <p className="ride-line"><span className="ride-line-span">or</span></p>
-            <Button variant="outline-primary" block>
-              Sign in with Google
+              <div style={{ margin: '40px 0px' }}>
+                <p className="ride-line"><span className="ride-line-span">or</span></p>
+                <Button variant="outline-primary" block>
+                  Sign in with Google
             </Button>
-            </div>
-          </Form>
-          <p style={{ paddingBottom: '100px'}}>
-            Don't have an account? Sign up <Link to='signUp'>here</Link>
-          </p>
+              </div>
+            </Form>
+            <p style={{ paddingBottom: '100px' }}>
+              Don't have an account? Sign up <Link to='signUp'>here</Link>
+            </p>
           </div>
         </Container>
       </div>
