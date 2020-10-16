5406f80076bae70892631fa93787812fe2b79bb9
ADC04 <allendcooper@gmail.com>
2020-10-16 14:53:48 -0400

    Updates

---
+++ precache-manifest.74896ee2bdd0a2bdf97da18aa74d4d37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df42efef55fc2b08a1cfd1f304eb89bf",
+    "url": "/StudyParty/index.html"
+  },
+  {
+    "revision": "b20bdb52289130c56e7b",
+    "url": "/StudyParty/static/css/2.994f7b2c.chunk.css"
+  },
+  {
+    "revision": "b1a05775f5adc1331fed",
+    "url": "/StudyParty/static/css/main.0b80e802.chunk.css"
+  },
+  {
+    "revision": "b20bdb52289130c56e7b",
+    "url": "/StudyParty/static/js/2.ab6e82b7.chunk.js"
+  },
+  {
+    "revision": "22c95ed68071a711843b9981e2bca1af",
+    "url": "/StudyParty/static/js/2.ab6e82b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1a05775f5adc1331fed",
+    "url": "/StudyParty/static/js/main.cde2570a.chunk.js"
+  },
+  {
+    "revision": "3e4e4ec8268dd4c7d68b",
+    "url": "/StudyParty/static/js/runtime-main.860b1cb1.js"
+  }
+]);
