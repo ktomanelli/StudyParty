19243744b29661d39335e22fa5e679f4479755bb
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-10-23 18:04:05 -0400

    added error message text above form on error

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,146 +1,114 @@
-import React, { Component } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Container, Jumbotron, Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { signInWithGoogle } from '../../firebase';
 import firebase from "firebase/app";
 
-class SignUp extends Component {
-  state = {
+const SignUp = props=>{
+  const [{typed,i},setTyped] = useState({typed:'',i:0})
+  const [userInput,setUserInput] = useState({
     email: "",
     password: "",
     displayName: "",
     error: null,
-    typed: "",
-  }
+  })
+  const subtitleStr = 'Hey! You want to umm... study???'
 
-  // when component mounts, will run typewriter effect on subtitle after 1.5s
-  componentDidMount() {
-    const subtitleStr = 'Hey! You want to umm... study???'
-    setTimeout(() => this.typeWriter(subtitleStr), 1500)
-  }
+  useEffect(()=>{
+    if(typed.length === subtitleStr.length) return
+    setTimeout(()=>{
+      setTyped({typed:typed+subtitleStr[i],i:i+1})
+    },Math.random()*200+50)
+  },[typed])
 
-  createUserWithEmailAndPasswordHandler = (event, email, password, displayName) => {
-    console.log(email);
-    console.log(password);
-    console.log(displayName);
+ const  createUserWithEmailAndPasswordHandler = () => {
+  // adds displayName immediately to state on Application.js to display on navigation bar
+  props.updateDisplayNameFromForm(userInput.displayName)
 
-    event.preventDefault();
-
-    // adds displayName immediately to state on Application.js to display on navigation bar
-    this.props.updateDisplayNameFromForm(this.state.displayName)
-
-    firebase.auth()
-      // .createUserWithEmailAndPassword(email, password)
-      .createUserWithEmailAndPassword(email, password)
-      .then(function () {
-        console.log('Successfully created new user');
-        var user = firebase.auth().currentUser;
-        user.updateProfile({
-          displayName: displayName
-        })
-          .then(function () {
-            console.log(`Successfully updated profile with displayName`)
-          })
-          .catch(function (error) {
-            console.log(`Error updating user with displayName:`, error)
-          });
+  firebase.auth()
+    .createUserWithEmailAndPassword(userInput.email, userInput.password)
+    .then(function () {
+      console.log('Successfully created new user');
+      const user = firebase.auth().currentUser;
+      user.updateProfile({
+        displayName: userInput.displayName
       })
-      .catch(function (error) {
-        console.log('Error creating new user:', error)
-      });
-
-    this.setState({
-      email: "",
-      password: "",
-      displayName: ""
+        .then(function () {
+          console.log(`Successfully updated profile with displayName`)
+        })
+        .catch(function (error) {
+          setUserInput({...userInput,error:error.message})
+          console.log(`Error updating user with displayName:`, error)
+        });
     })
-
+    .catch(function (error) {
+      setUserInput({...userInput,error:error.message})
+      console.log('Error creating new user:', error)
+    });
   };
 
   //function to handle form change
-  onChangeHandler = (event) => {
+  const handleChange = (event) => {
     const { name, value } = event.currentTarget;
-
     if (name === 'userEmail') {
-      this.setState({
-        email: value
-      })
+      setUserInput({...userInput,email:value})
     } else if (name === 'userPassword') {
-      this.setState({
-        password: value
-      })
+      setUserInput({...userInput,password:value})
     } else if (name === 'displayName') {
-      this.setState({
-        displayName: value
-      })
+      setUserInput({...userInput,displayName:value})
     }
   }
-
-  // function that will take add a letter from a given string to 'typed' string stored in state every 50 milliseconds
-  typeWriter(slicedStr) {
-    if (slicedStr.length !== 0) {
-      this.setState((state) => ({
-        typed: state.typed.concat(slicedStr[0])
-      }));
-      setTimeout(() => this.typeWriter(slicedStr.slice(1)), 50);
-    }
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    createUserWithEmailAndPasswordHandler()
   }
+  return (
+    <div>
+      <Container style={{ backgroundColor: 'white' }}>
 
-  render() {
-    return (
-      <div>
-        <Container style={{ backgroundColor: 'white' }}>
+        <Jumbotron style={{ backgroundColor: 'white', marginBottom: '0px', textAlign: "center" }}>
+          <h1 className="main-title permanent-marker pink animate__animated animate__bounceInDown">StudyParty!</h1>
+          <h6 className="montserrat">
+            {typed}
+            <span className="cursor">
+              <span style={{ fontWeight: "normal" }}>|</span>
+            </span>
+          </h6>
+        </Jumbotron>
+        <div style={{ maxWidth: "600px", margin: "0 auto" }}>
+        {userInput.error&&<p style={{color:'red'}}>{userInput.error}</p>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="userName">
+              <Form.Label>Display name</Form.Label>
+              <Form.Control type="name" placeholder="Enter name" name="displayName" value={userInput.displayName} onChange={handleChange} />
+            </Form.Group>
 
-          <Jumbotron style={{ backgroundColor: 'white', marginBottom: '0px', textAlign: "center" }}>
-            <h1 className="main-title permanent-marker pink animate__animated animate__bounceInDown">StudyParty!</h1>
-            <h6 className="montserrat">
-              {this.state.typed}
-              <span className="cursor">
-                <span style={{ fontWeight: "normal" }}>|</span>
-              </span>
-            </h6>
-          </Jumbotron>
-          <div style={{ maxWidth: "600px", margin: "0 auto" }}>
-            <Form>
-              <Form.Group controlId="userName">
-                <Form.Label>Display name</Form.Label>
-                <Form.Control type="name" placeholder="Enter name" name="displayName" value={this.state.displayName} onChange={(event) => this.onChangeHandler(event)} />
-              </Form.Group>
+            <Form.Group controlId="userEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" name="userEmail" value={userInput.email} onChange={handleChange} />
+            </Form.Group>
 
-              <Form.Group controlId="userEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" name="userEmail" value={this.state.email} onChange={(event) => this.onChangeHandler(event)} />
-              </Form.Group>
-
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" name="userPassword" value={this.state.password} onChange={(event) => this.onChangeHandler(event)} />
-              </Form.Group>
-              <Button
-                variant="primary"
-                type="submit"
-                block
-                onClick={event => {
-                  this.createUserWithEmailAndPasswordHandler(event, this.state.email, this.state.password, this.state.displayName);
-                }}
-              >
-                Sign Up
-            </Button>
-              <div style={{ margin: '40px 0px' }}>
-                <p className="ride-line"><span className="ride-line-span">or</span></p>
-                <Button onClick={signInWithGoogle} variant="outline-primary" block >
-                  Sign up with Google
-            </Button>
-              </div>
-            </Form>
-            <p style={{ paddingBottom: '100px' }}>
-              Already have an account? Sign in <Link to="/">here</Link>
-            </p>
-          </div>
-        </Container>
-      </div>
-    )
-  }
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" name="userPassword" value={userInput.password} onChange={handleChange} />
+            </Form.Group>
+            <Button variant="primary" type="submit" block>Sign Up</Button>
+            <div style={{ margin: '40px 0px' }}>
+              <p className="ride-line"><span className="ride-line-span">or</span></p>
+              <Button onClick={signInWithGoogle} variant="outline-primary" block >
+                Sign up with Google
+          </Button>
+            </div>
+          </Form>
+          <p style={{ paddingBottom: '100px' }}>
+            Already have an account? Sign in <Link to="/">here</Link>
+          </p>
+        </div>
+      </Container>
+    </div>
+  )
 }
+
+
 
 export default SignUp;
